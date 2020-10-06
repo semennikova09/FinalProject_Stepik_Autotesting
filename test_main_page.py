c64c0ff4637052b030766c210970480b16abf349
semennikova09 <semennikova09@gmail.com>
2020-10-06 21:17:34 +0300

    LoginPage

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,13 @@
-from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+from .pages.locators import LoginPageLocators
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
     browser.get(link)
-    page = MainPage(browser, link)
+    page = LoginPage(browser, link)
     page.open()
-    page.should_be_login_link()
+    #page.should_be_login_link()
+    page.should_be_login_url()
+    page.should_be_login_form()
+    page.should_be_register_form()
+    
